57f441cb4ba3d83d7e947b9e24158b04757d6bb1
Jesse Jurman <j.r.jurman@gmail.com>
2024-08-24 00:03:02 -0400

    custom npm-electron, webkit

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,21 @@ module.exports = defineConfig({
 		specPattern: '**.cy.js',
 		supportFile: false,
 		includeShadowDom: true,
+		experimentalWebKitSupport: true,
+
+		setupNodeEvents(on, config) {
+			return {
+				// include npm version of electron
+				browsers: config.browsers.concat({
+					name: 'npm-electron',
+					channel: 'stable',
+					family: 'chromium',
+					displayName: 'NPM Electron',
+					version: '32.0.1',
+					path: './node_modules/.bin/electron',
+					majorVersion: 32,
+				}),
+			};
+		},
 	},
 });
